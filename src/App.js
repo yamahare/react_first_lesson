9fbea4a0e18bdf685baefd47935ff6d5afb351e7
yamahare <jidai.norio@gmail.com>
2019-10-27 00:03:23 +0900

    [add]change app.js. add printMsg.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let printMsg = (msg, size, color) => {
+  const p_style = {
+    fontSize: `${size}px`,
+    color: `${color}`,
+    border: '1px solid blue'
+  }
+  return <p style={p_style}>{msg}</p>;
+}
+
 function App() {
+
+  const url = 'https://yamahare.github.io/react_first_lesson/';
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {printMsg('最初のメッセージ', 30, '#ddd')}
+        {printMsg('2番目メッセージ', 20, '#aaa')}
+        {printMsg('3番目メッセージ', 10, '#333')}
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={url}
           target="_blank"
           rel="noopener noreferrer"
         >
